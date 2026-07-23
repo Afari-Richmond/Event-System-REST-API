03a7e7212346c4a169bf7666202031799cd5f9bf
RICHMOND ADDO AFARI <macbookpro@Richmonds-MacBook-Pro.local>
2026-07-23 09:06:07 +0000

    (feat): Build the models for each data

---
+++ internal/database/atendees.go
@@ -0,0 +1,14 @@
+package database
+
+
+import "database/sql"
+
+type AttendeeModel struct {
+	DB *sql.DB
+}
+
+type Attendee struct {
+	Id      int `json:"id"`
+	EventId int `json:"eventId "`
+	UserId  int `json:"userId"`
+}
